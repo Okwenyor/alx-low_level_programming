ba2e769c2e5defb578f85d5d715cfec9507e7547
Okwenyor <victorial.apedoh@gmail.com>
2023-04-20 23:04:34 -0700

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- *main - Entry point
  * Return: Always 1 (Error)
  */
 int main(void)
 {
 	char *str = "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19n";
-
-fwrite(str, 1, sizeof(str), stderr);
-return 1;
+	int len = 0;
+	
+	while (str[len])
+	{
+	len++;
+}
+write(2, str, len);
+return(1);
 }
