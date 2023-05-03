f16c5e981ee50bac933da2ce4c1c0fe00514545f
Okwenyor <victorial.apedoh@gmail.com>
2023-05-02 19:19:20 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * set_int_to_98 - updates the value of an integer to 98.
+ * @n: a pointer to an integer.
+ *
+ * Return: void.
+ */
+void set_int_to_98(int *n)
+{
+	*n = 98;
+}
