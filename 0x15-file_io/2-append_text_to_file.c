3e1decc2eda9feed8a4e8ebe2628ceb08229c660
Okwenyor <Victorial.apedoh@gmail.com>
2023-06-12 18:40:43 -0700

    Speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, text_length = 0;
+	int fd, bytes_written, text_length = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,18 +25,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[text_length])
 			text_length++;
 
-		for (int i = 0; i < text_length; i++)
+		bytes_written = write(fd, text_content, text_length);
+		if (bytes_written == -1 || bytes_written != text_length)
 		{
-			if (_putchar(text_content[i]) == -1)
-			{
-				close(fd);
-				return (-1);
-			}
+			close(fd);
+			return (-1);
 		}
 	}
 
 	close(fd);
 	return (1);
 }
-
 
