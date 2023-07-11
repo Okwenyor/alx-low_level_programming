7c073edd02ab62c6b86073a92568008053488d56
Okwenyor <Victorial.apedoh@gmail.com>
2023-07-11 00:52:01 -0700

    speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 
 /**
-* append_text_to_file - appends the given text content to an existing file
-* @filename : the name of the file
-* @text_content : NULL terminated string to add at the end of the file
-* Return: 1 on success and -1 on failure
-*/
+ * append_text_to_file - appends the given text content to an existing file
+ * @filename: the name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, wrlen;
@@ -18,14 +19,14 @@ return (-1);
 
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
-return (–1);
+return (-1);
 
 if (text_content != NULL)
 {
-for (text_content[text_len] != '\0')
+while (text_content[text_len] != '\0')
 text_len++;
 
-wrlen = write(fd, text_content, i);
+wrlen = write(fd, text_content, text_len);
 if (wrlen == -1)
 {
 close(fd);
