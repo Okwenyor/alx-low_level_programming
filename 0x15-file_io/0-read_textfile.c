c21e4b6cf317dcef969dd6d9bafb5af272b9c102
Okwenyor <Victorial.apedoh@gmail.com>
2023-07-10 23:46:22 -0700

    thread lightly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,32 +12,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char buffer[1024];
-	ssize_t bytes_read, total_bytes = 0;
 
-	if (filename == NULL)
-		return (0);
+int fd, rdlen;
+char buffer[1024];
+size_t total = 0;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	while (letters > 0 && (bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			close(fd);
-			return (0);
-		}
-
-		letters -= bytes_written;
-		total_bytes += bytes_written;
-	}
-
-	close(fd);
-	return (total_bytes);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+while (total < letters && (rdlen = read(fd, buffer, 1)) > 0)
+{
+if (write(STDOUT_FILENO, buffer, rdlen) != rdlen)
+{
+close(fd);
+return (0);
 }
+total++;
+}
+
+close(fd);
+return (total);
+}
+
 
