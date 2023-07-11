74cbcf483866bcb8c85b8ca40cd83fedb149e59e
Okwenyor <Victorial.apedoh@gmail.com>
2023-07-11 00:14:08 -0700

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,10 +3,10 @@
 #include <unistd.h>
 
 /**
-* create_file: creates a file and writes the given text content to it
+ * create_file - creates a file and writes the given text content
 * @filename: the name of the file to create
 * @text_content : a NULL terminated string to write to the file
-* Return : 1 on success, -1 on failure
+* Return: 1 on success or -1 on failure
 */
 int create_file(const char *filename, char *text_content)
 {
